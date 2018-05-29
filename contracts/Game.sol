4d6e8e1eb0ddafb3ca304846616030ad4b2afad9
Oliver Machačík <mail@oliver-machacik.com>
2018-05-29 17:37:27 +0200

    initial commit

---
+++ contracts/Game.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.4.17;
+
+contract Game {
+struct gameInfo {
+  uint size;
+  address[5] playerAddrs;
+}
+mapping(string => gameInfo) private playerGameMap;
+
+function join(string gameName, uint256 players) public returns (address[5]) {
+
+    if (playerGameMap[gameName].size == 0) {
+        require(players >= 2 && players <= 5);
+        playerGameMap[gameName] = gameInfo(players,[msg.sender,0,0,0,0]);
+        return playerGameMap[gameName].playerAddrs;
+    }
+
+    for(uint i=0;i<=playerGameMap[gameName].size;i++) {
+        if(playerGameMap[gameName].playerAddrs[i] == msg.sender){
+            break;
+        }
+        if(playerGameMap[gameName].playerAddrs[i] == 0){
+            playerGameMap[gameName].playerAddrs[i] = msg.sender;
+            break;
+        }
+    }
+
+    return playerGameMap[gameName].playerAddrs;
+}
+
+function getPlayers(string gameName) public view returns (address[5]) {
+    return playerGameMap[gameName].playerAddrs;
+}
+
+}
+
