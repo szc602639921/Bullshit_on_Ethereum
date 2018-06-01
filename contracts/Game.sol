28549d543f807f18e92e0c4d8abd1a8809c1ca3a
Unknown <pascal.zenker@rwth-aachen.de>
2018-06-01 11:17:16 +0200

    Auto stash before merge of "master" and "origin/master"

--- contracts/Game.sol
+++ contracts/Game.sol
@@ -1,36 +1,37 @@
 pragma solidity ^0.4.17;
 
 contract Game {
-struct gameInfo {
-  uint size;
-  address[5] playerAddrs;
-}
-mapping(string => gameInfo) private playerGameMap;
+    struct gameInfo {
+        uint size;
+        address[5] playerAddrs;
+    }
 
-function join(string gameName, uint256 players) public returns (address[5]) {
+    mapping(string => gameInfo) private playerGameMap;
 
-    if (playerGameMap[gameName].size == 0) {
-        require(players >= 2 && players <= 5);
-        playerGameMap[gameName] = gameInfo(players,[msg.sender,0,0,0,0]);
+    function join(string gameName, uint256 players) public returns (address[5]) {
+
+        if (playerGameMap[gameName].size == 0) {
+            require(players >= 2 && players <= 5);
+            playerGameMap[gameName] = gameInfo(players,[msg.sender,0,0,0,0]);
+            return playerGameMap[gameName].playerAddrs;
+        }
+
+        for(uint i = 0; i<=playerGameMap[gameName].size; i++) {
+            if(playerGameMap[gameName].playerAddrs[i] == msg.sender){
+                break;
+            }
+            if(playerGameMap[gameName].playerAddrs[i] == 0){
+                playerGameMap[gameName].playerAddrs[i] = msg.sender;
+                break;
+            }
+        }
+
         return playerGameMap[gameName].playerAddrs;
     }
 
-    for(uint i=0;i<=playerGameMap[gameName].size;i++) {
-        if(playerGameMap[gameName].playerAddrs[i] == msg.sender){
-            break;
-        }
-        if(playerGameMap[gameName].playerAddrs[i] == 0){
-            playerGameMap[gameName].playerAddrs[i] = msg.sender;
-            break;
-        }
+    function getPlayers(string gameName) public view returns (address[5]) {
+        return playerGameMap[gameName].playerAddrs;
     }
-
-    return playerGameMap[gameName].playerAddrs;
-}
-
-function getPlayers(string gameName) public view returns (address[5]) {
-    return playerGameMap[gameName].playerAddrs;
-}
 
 function isGameFull(string gameName) public view returns (bool) {
     gameInfo memory currentGame =  playerGameMap[gameName];
