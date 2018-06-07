24fd4b890944ed801c9ea77b28a95e1c9e744f26
Oliver Machačík <mail@oliver-machacik.com>
2018-06-07 16:36:05 +0200

    improved code readability

--- contracts/Game.sol
+++ contracts/Game.sol
@@ -15,20 +15,21 @@ contract Game {
 
     function join(string gameName, uint256 players) public returns (address[5]) {
 
-        if (playerGameMap[gameName].size == 0) {
+        gameInfo memory currentGame =  playerGameMap[gameName];
+
+        if (currentGame.size == 0) {
             require(players >= 2 && players <= 5);
             playerGameMap[gameName] = gameInfo(players, [msg.sender, 0, 0, 0, 0], 5);
 
             return playerGameMap[gameName].playerAddrs;
         }
 
-        gameInfo memory currentGame =  playerGameMap[gameName];
 
         for (uint i = 0; i <= currentGame.size; i++) {
-            if (playerGameMap[gameName].playerAddrs[i] == msg.sender) {
+            if (currentGame.playerAddrs[i] == msg.sender) {
                 break;
             }
-            if (playerGameMap[gameName].playerAddrs[i] == 0x0) {
+            if (currentGame.playerAddrs[i] == 0x0) {
                 if (i == currentGame.size - 1) {
                     uint rnd = uint(keccak256(block.timestamp)) % currentGame.size;
                     playerGameMap[gameName].dealer = rnd;
@@ -38,7 +39,7 @@ contract Game {
             }
         }
 
-        return playerGameMap[gameName].playerAddrs;
+        return currentGame.playerAddrs;
     }
 
     function getPlayers(string gameName) public view returns (address[5]) {
